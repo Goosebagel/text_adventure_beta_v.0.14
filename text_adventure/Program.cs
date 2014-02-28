b55f44aae1aac86c80a0effd53abd8f94bed0a76
Goosebagel <goosebagelftw@gmail.com>
2014-02-27 19:18:34 -0800

    These are all the files
    nein

---
+++ text_adventure/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace text_adventure
+{
+    class the_beginning
+    {
+        static void Main(string[] args)
+        {
+            string name;
+
+            Console.WriteLine("Welcome to Goosebagel's AMAZING Text Adventure.");
+
+            Console.WriteLine("This is my first one so go easy one me, pls.");
+
+            Console.WriteLine("First things first, whats your name?");
+
+            name = Console.ReadLine();
+
+            Console.WriteLine("So your name is {0}?",name);
+
+            Console.WriteLine("Thats a stupid name but anyways lets move on...");
+
+            Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine(@"Thanks for playing the beta!!!
+
+I know you enjoyed as much as I enjoyed programming it, gg 
+
+ O and if you enjoyed this adventure or want more contact the dev: Goosebagel");
+
+            Console.ReadLine();
+
+        }
+    }
+}
